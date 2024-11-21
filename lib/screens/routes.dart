afc0a81eb3ad939f236b5670bcd8c2f04a28f1d9
Samiul Hossen Sarkar Santo <samiulhossen2254901079@student.bup.edu.bd>
2024-11-21 21:46:25 +0600

    made the images zoomable and center-fit

--- lib/screens/routes.dart
+++ lib/screens/routes.dart
@@ -17,20 +17,27 @@ class _RoutesState extends State<Routes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Bus Tracker',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.green[900],
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          'Bus Tracker',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: widget.routeModel.image.isNotEmpty
-                ? Image.asset(
-                    "assets/images/${widget.routeModel.image}", // Adjust the path to your folder
+        backgroundColor: Colors.green[900],
+        centerTitle: true,
+      ),
+      body: widget.routeModel.image.isNotEmpty
+          ? Center(
+              child: InteractiveViewer(
+                onInteractionUpdate: (ScaleUpdateDetails details) {
+                  debugPrint("Scale: ${details.scale}");
+                },
+                panEnabled: true, // Enable panning
+                minScale: 1.0, // Minimum zoom scale
+                maxScale: 5.0, // Maximum zoom scale
+                child: Align(
+                  alignment: Alignment.center, // Center the image
+                  child: Image.asset(
+                    "assets/images/${widget.routeModel.image}",
                     errorBuilder: (context, error, stackTrace) {
                       debugPrint(
                           "Error loading image: ${widget.routeModel.image}");
@@ -38,11 +45,15 @@ class _RoutesState extends State<Routes> {
                         child: Text("Image not available"),
                       );
                     },
-                  )
-                : const Center(
-                    child: Text("No image provided"),
+                    fit: BoxFit
+                        .contain, // Adjust the image to fit within the screen
                   ),
-          ),
-        ));
+                ),
+              ),
+            )
+          : const Center(
+              child: Text("No image provided"),
+            ),
+    );
   }
 }
